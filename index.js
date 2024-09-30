2ad708f78a8d87469bdef48502c554166aebbd51
Artemijs Matusev <matu6ev@gmail.com>
2024-09-30 14:58:07 +0100

    Set up submodule for the front-end MonkeyPuzzle data

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
+
+//Setting up middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/Public/index.html") 
+})
 
 app.listen(3000, () => {
     console.log("Server running on port 3000.");
